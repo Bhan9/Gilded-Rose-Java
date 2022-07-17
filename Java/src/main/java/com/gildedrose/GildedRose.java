ccc0e81edca63a11a97e24c288ded2b8e712753e
Bhan9 <96214927+Bhan9@users.noreply.github.com>
2022-07-17 18:57:29 -0300

    Issue-5: Initial TODO stubs
    Refactor of the updateQuality method to be better all round to allow future modifications.

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -2,11 +2,111 @@ package com.gildedrose;
 
 class GildedRose {
     Item[] items;
+    
+    //TODO Issue-5: Add hardcoded item type Strings for comparison
+    //TODO Issue-5: Add hardcoded quality cap constraint numbers (max, min, legendary)
+    
 
     public GildedRose(Item[] items) {
+    	
+
+    	
         this.items = items;
+      //TODO Issue-5: Validate quality constraints
+        //this.items = validateItem(items);
+      
     }
 
+    
+    
+    
+    public void updateQuality() {
+    	//TODO Issue-5: Rewrite completely.
+    	
+    	for (int i = 0; i < items.length; i++) {
+    	
+    		Item currentItem = items[i];
+    		//TODO Issue-5: Determine Item Type
+    		//String itemType = getItemType(currentItem);
+    		
+    		
+	    	//TODO Issue-5: Process by Item Type
+    	
+    	
+    	}
+    	
+    }
+    
+    //TODO Issue-5: Implement.
+    /*public void updateBasicItem(Item item) {
+     * 
+	}*/
+    
+    
+    //TODO Issue-5: Implement.
+    /*public void updateAgedBrieItem(Item item) {
+     * 
+	}*/
+    
+    
+    //TODO Issue-5: Implement.
+    /*public void updateBackstagePassItem(Item item) {
+     * 
+	}*/
+    
+    
+    //TODO Issue-5: Implement.
+    /*public void updateSulfurasItem(Item item) {
+     * 
+	}*/
+    
+    //TODO Issue-5: Implement.
+    /*private Item[] validateItems(Item item){
+     * 
+     *     	for (int i = 0; i < items.length; i++) {
+        	
+    		Item currentItem = items[i];
+    		//TODO Issue-5: Determine Item Type
+    		//items[i] = validateItem(items[i]);
+    		//Sulfuras: quality to 80
+    		//Not Sulfuras: quality capped to 50 if above, increased to 0 if below
+    		//Backstage pass: Quality to 0 if sellIn is negative
+    		
+    	
+    	
+    	}
+     * 
+     * 
+     }*/
+    
+    
+    //TODO Issue-5: Implement.
+    /*private String getItemType(Item item){
+     * 
+     * 
+     }*/
+    
+    
+    //TODO Issue-2: Implement. Cast the array to an Arraylist, call list.add, cast the Arraylist back to an array
+    //the turn the items array into a new array created from list.toArray. Make sure not to lose order of the array
+    //The New item should always be at the end of the array, with the array size going up by 1.
+    /*public void addToItems(Item item){
+     * 
+     * }
+     */
+    
+    //TODO Issue-2: Implement. Cast the array to an Arraylist, call list.remove(reference), cast the Arraylist back to an array
+    //the turn the items array into a new array created from list.toArray. Make sure not to lose order of the array
+    //Items before the removed item should maintain their place in the array, items after the removed item should have moved up one spot,
+    //with the array size decreasing by 1.
+    /*public void removeFromItems(int reference){
+     * 
+     * }
+     */
+    
+    
+    //TODO Issue-5: Delete this method.
+    /*
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie")
@@ -58,5 +158,7 @@ class GildedRose {
                 }
             }
         }
-    }
+    }*/
+    
+    
 }

b9f8ec12c6c52f35382253526045d86267ed0681
Bhan9 <96214927+Bhan9@users.noreply.github.com>
2022-07-17 20:00:09 -0300

    Issue-5: Refactor of UpdateQuality

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,90 +1,185 @@
 package com.gildedrose;
 
+import java.util.logging.Logger;
+
 class GildedRose {
     Item[] items;
     
-    //TODO Issue-5: Add hardcoded item type Strings for comparison
-    //TODO Issue-5: Add hardcoded quality cap constraint numbers (max, min, legendary)
+    private Logger log = Logger.getLogger(GildedRose.class.getName());
+    
+    private final String SULFURAS = "sulfuras";
+    private final String BACKSTAGEPASS = "backstage pass";
+    private final String AGEDBRIE = "aged brie";
+    private final String BASIC = "basic";
+    
+    private final int LEGENDARYQUALITY = 80;
+    private final int MAXQUALITY = 50;
+    private final int MINQUALITY = 0;
     
 
     public GildedRose(Item[] items) {
-    	
-
-    	
-        this.items = items;
-      //TODO Issue-5: Validate quality constraints
-        //this.items = validateItem(items);
-      
+        this.items = validateItems(items);
     }
 
-    
-    
-    
-    public void updateQuality() {
-    	//TODO Issue-5: Rewrite completely.
-    	
+    public void updateQuality() {    	
     	for (int i = 0; i < items.length; i++) {
     	
     		Item currentItem = items[i];
-    		//TODO Issue-5: Determine Item Type
-    		//String itemType = getItemType(currentItem);
+    		String itemType = getItemType(currentItem);
     		
-    		
-	    	//TODO Issue-5: Process by Item Type
-    	
-    	
+    		if (itemType.equalsIgnoreCase(BASIC)) {
+    			items[i] = updateBasicItem(currentItem);
+    		}
+    		else if (itemType.equalsIgnoreCase(AGEDBRIE)){
+    			items[i] = updateAgedBrieItem(currentItem);
+    			
+    		}
+    		else if (itemType.equalsIgnoreCase(BACKSTAGEPASS)){
+    			items[i] = updateBackstagePassItem(currentItem);
+    		}
+    		else if (itemType.equalsIgnoreCase(SULFURAS)){
+    			items[i] = updateSulfurasItem(currentItem);
+    		}
+    		else {
+    			log.warning("Unidentified Item type during update: " + itemType + " for Item: " + currentItem.toString());
+    		}
+	
     	}
     	
     }
     
-    //TODO Issue-5: Implement.
-    /*public void updateBasicItem(Item item) {
-     * 
-	}*/
-    
-    
-    //TODO Issue-5: Implement.
-    /*public void updateAgedBrieItem(Item item) {
-     * 
-	}*/
-    
-    
-    //TODO Issue-5: Implement.
-    /*public void updateBackstagePassItem(Item item) {
-     * 
-	}*/
-    
-    
-    //TODO Issue-5: Implement.
-    /*public void updateSulfurasItem(Item item) {
-     * 
-	}*/
-    
-    //TODO Issue-5: Implement.
-    /*private Item[] validateItems(Item item){
-     * 
-     *     	for (int i = 0; i < items.length; i++) {
+    private Item[] validateItems(Item[] items){
+     
+         	for (int i = 0; i < items.length; i++) {
         	
     		Item currentItem = items[i];
-    		//TODO Issue-5: Determine Item Type
-    		//items[i] = validateItem(items[i]);
-    		//Sulfuras: quality to 80
-    		//Not Sulfuras: quality capped to 50 if above, increased to 0 if below
-    		//Backstage pass: Quality to 0 if sellIn is negative
+    		String itemType = getItemType(currentItem);
     		
-    	
-    	
+    		if (itemType.equalsIgnoreCase(BASIC)) {
+    			items[i] = validateBasicItem(currentItem);
+    		}
+    		else if (itemType.equalsIgnoreCase(AGEDBRIE)){
+    			items[i] = validateAgedBrieItem(currentItem);
+    			
+    		}
+    		else if (itemType.equalsIgnoreCase(BACKSTAGEPASS)){
+    			items[i] = validateBackstagePassItem(currentItem);
+    		}
+    		else if (itemType.equalsIgnoreCase(SULFURAS)){
+    			items[i] = validateSulfurasItem(currentItem);
+    		}
+    		else {
+    			log.warning("Unidentified Item type during validation: " + itemType + " for Item: " + currentItem.toString());
+    		} 	
     	}
-     * 
-     * 
-     }*/
+         	
+        return items;
+     }
     
+    private Item validateMinQuality(Item item) {
+		if (item.quality < MINQUALITY) {
+			item.quality = MINQUALITY;
+		}
+		return item;
+    }
     
-    //TODO Issue-5: Implement.
-    /*private String getItemType(Item item){
-     * 
-     * 
-     }*/
+    private Item validateMaxQuality(Item item) {
+    	if (item.quality > MAXQUALITY) {
+			item.quality = MAXQUALITY;
+		}
+    	return item;
+    }
+    
+    private Item itemInStandardQualityRange(Item item) {
+    	item = validateMinQuality(item);
+    	return validateMaxQuality(item);
+    }
+    
+    private Item validateBasicItem(Item item) {
+    	return itemInStandardQualityRange(item);
+    }
+    
+    private Item validateAgedBrieItem(Item item) {
+    	return itemInStandardQualityRange(item);
+    }
+    
+    private Item validateBackstagePassItem(Item item) {
+    	if (item.sellIn < 0) {
+    		item.quality = 0;
+    	}
+    	return itemInStandardQualityRange(item);
+    }
+    
+    private Item validateSulfurasItem(Item item) {
+    	item.quality = LEGENDARYQUALITY;
+		return item;
+	}
+    
+    private Item updateSulfurasItem(Item item) {
+    	item.quality = LEGENDARYQUALITY;
+		return item;
+	}
+
+	private Item updateBackstagePassItem(Item item) {
+		//If sellIn is equal to zero, the event is tonight, so we do not zero out until the day after the event
+		if (item.sellIn < 0) {
+			item.quality = 0;
+		}
+		else if (item.sellIn < 6) {
+			item.quality+=3;
+		}
+		else if (item.sellIn < 11) {
+			item.quality+=2;
+		}
+		else {
+			item.quality+=1;
+		}
+		
+		item.sellIn--;
+		
+		return validateMaxQuality(item);
+	}
+
+
+	private Item updateAgedBrieItem(Item item) {
+		item.quality++;		
+		
+		//TODO Issue-8: Possible removal
+		item.sellIn--;
+		
+		return validateMaxQuality(item);
+	}
+
+	private Item updateBasicItem(Item item) {
+		if (item.sellIn < 0) {
+			item.quality -=2 ;
+		}
+		else {
+			item.quality--;
+		}
+		
+		item.sellIn--;		
+		
+		return validateMinQuality(item);
+	}
+    
+    //TODO Issue-7: Confirm order of precedence for Item Type
+    private String getItemType(Item item){
+    	String itemType = BASIC;
+    	String itemName = item.name.toLowerCase();
+    	
+    	if (itemName.contains(SULFURAS)) {
+    		itemType = SULFURAS;
+    	}
+    	else if (itemName.contains(BACKSTAGEPASS)) {
+    		itemType = BACKSTAGEPASS;
+    	}
+    	else if (itemName.contains(AGEDBRIE)) {
+    		itemType = AGEDBRIE;
+    	}
+    	
+    	return itemType;
+    }
     
     
     //TODO Issue-2: Implement. Cast the array to an Arraylist, call list.add, cast the Arraylist back to an array
@@ -103,62 +198,5 @@ class GildedRose {
      * 
      * }
      */
-    
-    
-    //TODO Issue-5: Delete this method.
-    /*
-    public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
-        }
-    }*/
-    
     
 }
